cc0581b697445eabd3f8fe5315b4402d1051d88f
VPagent <vpagent13@gmail.com>
2023-09-12 00:42:37 +0300

    try to fix

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/">
+      <BrowserRouter basename="https://vpagent.github.io/portfolio/">
         <App />
       </BrowserRouter>
     </Provider>
