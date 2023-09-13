e8afee827ba79c1623bb7e1ba19f81754731655c
VPagent <vpagent13@gmail.com>
2023-09-13 16:38:16 +0300

    try to build

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="https://vpagent.github.io/portfolio/">
+      <BrowserRouter basename="https://VPagent.github.io/portfolio/">
         <App />
       </BrowserRouter>
     </Provider>
